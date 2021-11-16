0ba1c13b9ec213208a474962ca10a364bf90b6c6
Joshua Robertson <joshrobertson774@gmail.com>
2021-11-16 20:55:09 +0000

    Fixed nav bar centering issues on low width window.

--- src/nav.js
+++ src/nav.js
@@ -1,6 +1,11 @@
-$(document).ready(function () {
-	$('#main-nav').click(function () {
-		if (this.className === "collapsible") { this.className += " responsive"; }
-		else { this.className = "collapsible"; }
+$(document).ready(function(){
+	$("#main-nav").click(function(){
+		if(this.className === "collapsible") this.className += " responsive";
+		else this.className = "collapsible";
 	});
+});
+
+$(window).resize(function(){
+ 	if($(window).width() < 575) $(".nav-wrapper").removeClass("centerise");
+ 	else $(".nav-wrapper").addClass("centerise");
 });
