22c56d47610579b24ae98c396da0cadbc77a0717
Joshua Robertson <joshrobertson774@gmail.com>
2021-11-16 20:59:15 +0000

    Fixed another issue with centering the nav bar.

--- src/nav.js
+++ src/nav.js
@@ -1,11 +1,16 @@
-$(document).ready(function(){
-	$("#main-nav").click(function(){
-		if(this.className === "collapsible") this.className += " responsive";
-		else this.className = "collapsible";
-	});
+function adjustNavBar() {
+    if($(window).width() < 575) $(".nav-wrapper").removeClass("centerise");
+    else $(".nav-wrapper").addClass("centerise");
+}
+
+$(window).resize(function() {
+    adjustNavBar();
 });
 
-$(window).resize(function(){
- 	if($(window).width() < 575) $(".nav-wrapper").removeClass("centerise");
- 	else $(".nav-wrapper").addClass("centerise");
+$(document).ready(function() {
+    adjustNavBar();
+    $("#main-nav").click(function() {
+        if(this.className === "collapsible") this.className += " responsive";
+        else this.className = "collapsible";
+    });
 });
