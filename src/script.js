41f0e1e3ef8a3cf02203b12593453acb88034abb
Joshua Robertson <joshrobertson774@gmail.com>
2021-11-17 17:18:49 +0000

    Big overhaul and started implementing game section content.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,15 @@
+function toggleGameContent (id) {
+    var banner = document.getElementById(id + "-banner");
+    var content = document.getElementById(id + "-content");
+    if(content.style.display === "none" || content.style.display === "") {
+        banner.style.display = "none";
+        content.style.display = "block";
+    } else {
+        banner.style.display = "block";
+        content.style.display = "none";
+    }
+}
+
 function adjustNavBar() {
     let nav = document.getElementById("nav-wrapper");
     if(window.innerWidth > 575) nav.className = "center";
